cdc534037a29302c0aa5ccad7cb9b6f39af50f79
zhenyi4 <zhenyi.shen@kcl.ac.uk>
2025-08-21 15:34:15 +0000

    update commonsense llama training script

--- scripts/train_llama_commonsense.sh
+++ scripts/train_llama_commonsense.sh
@@ -1,23 +1,23 @@
-SAVE_DIR=/ephemeral/gpt2_commonsense
+SAVE_DIR=~/codi_ckpt/llama_commonsense
 
 mkdir -p "$SAVE_DIR"
 
-cp scripts/train_gpt2_commonsense.sh "$SAVE_DIR"
+cp scripts/train_llama_commonsense.sh "$SAVE_DIR"
 
 python train.py \
 	--output_dir "$SAVE_DIR" \
   	--expt_name gsm8k_llama1b_latent_baseline \
 	--logging_dir "$SAVE_DIR/logs"\
 	--logging_steps 10 \
-	--model_name_or_path gpt2 \
+	--model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
 	--data_name commonsense \
 	--seed 11 \
 	--model_max_length 512 \
-	--per_device_train_batch_size 64 \
+	--per_device_train_batch_size 32 \
   	--gradient_accumulation_steps 2 \
 	--bf16 \
-	--num_train_epochs 50 \
-	--learning_rate 3e-3 \
+	--num_train_epochs 3 \
+	--learning_rate 8e-4 \
 	--max_grad_norm 2.0 \
 	--lora_r 128 --lora_alpha 32 --lora_init \
 	--save_strategy "no" \
@@ -31,10 +31,11 @@ python train.py \
     --num_latent 6 \
     --logging_strategy "steps" \
 	--use_prj True \
-	--prj_dim 768 \
+	--prj_dim 2048 \
 	--prj_dropout 0.0 \
-	--mse_loss_div_std True \
+	--distill_loss_div_std True \
 	--exp_mode False \
 	--exp_data_num 200 \
 	--remove_eos True \
 	--print_ref_model_stats True \
+	--distill_loss_factor 20 \
